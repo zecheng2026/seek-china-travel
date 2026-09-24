380bb4d67845a4e0869654e2a3b6f7c2ab0e88a0
zecheng2026 <15907448636@163.com>
2026-09-24 15:47:38 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,10 @@
 import type { NextConfig } from "next";
-const nextConfig: NextConfig = { images: { unoptimized: true } };
+
+const nextConfig: NextConfig = {
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+};
+
 export default nextConfig;
